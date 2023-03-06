bed6312c19a9f42d451bfc61c6a8bdbc8a6e627f
abdul <malik301002@gmail.com>
2023-03-06 21:01:39 +0700

    chore: edit lint-staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,12 +1,9 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': 'pnpm tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
 
   // Lint & Prettify TS and JS files
-  '**/*.(js|cjs|mjs|ts|tsx)': [
-    'pnpm eslint',
-    'pnpm prettier --w',
-  ],
+  '**/*.(js|cjs|mjs|ts|tsx)': ['pnpm eslint', 'pnpm prettier --w'],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': 'pnpm prettier --w',
